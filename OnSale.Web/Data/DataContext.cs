03d68ae85c37d8bd5dcce97fc80661e26485bbee
kevens herbyson philippe <kevensherbyson@gmail.com>
2020-11-04 00:25:25 -0300

    finished to add table of (pedidos)

--- OnSale.Web/Data/DataContext.cs
+++ OnSale.Web/Data/DataContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using OnSale.Common.Entities;
+using OnSale.Web.Data.Entities;
 
 namespace OnSale.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext :  IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -14,6 +16,11 @@ namespace OnSale.Web.Data
         public DbSet<City> Cities { get; set; }
 
         public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
         public DbSet<Product> Products { get; set; }
 
